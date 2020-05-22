7016fddb76371a367a52beec13b10664c90d0d0b
Joe <jparkkennaby@gmail.com>
2020-05-22 00:07:09 +0000

    SUPRD-250 - stop js being enqueued

--- functions/enqueue-scripts.php
+++ functions/enqueue-scripts.php
@@ -6,7 +6,7 @@ function simple_popups_enqueue_scripts() {
 
     wp_enqueue_style('simple-popups', simple_popup_plugin_url('includes/css/style.css'), array(), SIMPLE_POPUP_VERSION, 'all');
 
-    wp_enqueue_script('simple-popups', simple_popup_plugin_url('includes/js/all.min.js'), array('jquery'), SIMPLE_POPUP_VERSION, true);
+//    wp_enqueue_script('simple-popups', simple_popup_plugin_url('includes/js/all.min.js'), array('jquery'), SIMPLE_POPUP_VERSION, true);
 
 
 }
